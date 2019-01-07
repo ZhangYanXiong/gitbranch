8fb072b988625ac5edd11b203d7796e11d607246
2283609260@qq.com <2283609260@qq.com>
2019-01-07 22:23:44 +0800

    修改

--- leadingf/src/components/Home.vue
+++ leadingf/src/components/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
       <Mheader>
         <i class="iconfont icon-ditu"></i>
-        <span>北京</span>
+        <span>上海</span>
         <div class="input_wrap">
           <img src="../imgs/logo.png">
           <el-input 
